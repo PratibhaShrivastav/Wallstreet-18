e34678b63c8042131ef3727bc0fa048e6e671003
PratibhaShrivastav <pratibha.shrivastav99@gmail.com>
2018-08-31 19:08:11 +0530

    Dashboard graph integrated

--- Dashboard/charts.js
+++ Dashboard/charts.js
@@ -1,25 +1,75 @@
 //var mychart=document.getElementById("chart").getContext("2d");
+var labelsss,datass;
+$(document).ready(function(){
+  $.ajax({
+    type:'GET',
+    url: 'https://wallstreet-18.herokuapp.com/sellbuy/graph/53',
+    dataType:'json',
+    success: function(data) {
+       labelsss= data.map(function(e){
+        return e.y;
+      }).get();
+       datass= data.map(function(e){
+        return e.x;
+      }).get();
+    }
+  });
+});
+Chart.defaults.global.defaultFontColor = 'gray';
+Chart.defaults.global.defaultFontFamily ="'Signika', sans-serif";
+Chart.defaults.global.defaultFontSize = 12;
 var ctx = document.getElementById("c").getContext("2d");
   var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-      label: "My First dataset",
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }, {
-      label: "My Second dataset",
-      fillColor: "rgba(151,187,205,0.2)",
-      strokeColor: "rgba(151,187,205,1)",
-      pointColor: "rgba(151,187,205,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(151,187,205,1)",
-      data: [28, 48, 40, 19, 86, 27, 90]
+    type:'line',
+    data: {
+      labels:/*['10:00am','11:00am', '12:00am', '1:00am', '2:00am', '3:00am','4:00am', '5:00am'],*/labelsss,
+      datasets:[{
+        label:'You',
+        /*[
+          259.2,
+          267.3,
+          290.0,
+          289.6,
+          301.3,
+          300.0,
+          310.0,
+          311.9
+        ],*/
+        data: datass,
+        fill: false,
+        borderColor: "red",
+        pointHoverBackgroundColor : "red",
+        borderWidth: "1px",
+      }]
+    },
+  }
+  var data1= {
+    type:'line',
+  data: {
+    labels:['10:00am','11:00am', '12:00am', '1:00am', '2:00am', '3:00am','4:00am', '5:00am'],
+    datasets:[{
+      label:'Amount',
+      data:[
+        340.0,
+        350.8,
+        345.2,
+        347.3,
+        350.0,
+        349.7,
+        359.0,
+        357.2
+      ],
+      fill: false,
+      borderColor: "red",
+      pointHoverBackgroundColor : "red",
+      borderWidth: "1px",
     }]
-  };
-  var MyNewChart = new Chart(ctx).Line(data);
+  },
+}
+var myNewChart = new Chart(ctx, data);
+function displayfirst(){
+var myNewChart = new Chart(ctx, data);
+}
+function displaysecond(){
+var secondchart = new Chart(ctx, data1);
+}
